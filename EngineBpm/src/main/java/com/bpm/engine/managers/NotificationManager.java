f14e7a07acfb483d883cc951ea7968fe3b6f8210
Alejandro subero <alejandrosubero.ar@gmail.com>
2024-10-04 23:07:47 -0400

    map

--- EngineBpm/src/main/java/com/bpm/engine/managers/NotificationManager.java
+++ EngineBpm/src/main/java/com/bpm/engine/managers/NotificationManager.java
@@ -1,25 +1,45 @@
 package com.bpm.engine.managers;
 
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.bpm.engine.model.InstanceAbstractionModel;
 import com.bpm.engine.notification.services.NotificationService;
 
+@Component
 public class NotificationManager {
 
-	private NotificationService service; 
-	
-	
-	
-	@Au
+	private static NotificationService service; 
+	private ConcurrentHashMap<Long, InstanceAbstractionModel> concurrentHashMap = new ConcurrentHashMap<Long, InstanceAbstractionModel>();
+		
+	@Autowired
 	public NotificationManager(NotificationService service) {
-		super();
 		this.service = service;
 	}
 
-
-
-
-	public void notify(InstanceAbstractionModel instanceTask, String errorStatus) {
-		
-	
+	public static void notify(InstanceAbstractionModel instanceTask, String errorStatus) {
+		service.taskNotification(instanceTask, errorStatus);
 	}
+
+	
+	public void putInstanve(InstanceAbstractionModel instance) {
+		this.concurrentHashMap.put(instance.getIdInstance(), instance);
+	}
+	
+	
+	public InstanceAbstractionModel getInstance(Long instanceId) {
+		return this.concurrentHashMap.get(instanceId);
+	}
+	
+	public Boolean keyExist(Long instanceId) {
+		return this.concurrentHashMap.contains(instanceId);
+	}
+	
+	public Boolean removeInstance(Long instanceId) {
+		return this.concurrentHashMap.remove(instanceId) != null;
+	}
+	
+	
 }
